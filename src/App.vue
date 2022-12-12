03ab0bd5e27887e112f117a0838f6db8a34c78c2
Matheus de Souza <matheusdesouza187@gmail.com>
2022-12-12 08:12:56 -0300

    Primeiro commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <formulario-component></formulario-component>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import FormularioComponent from '@/components/FormularioComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FormularioComponent
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
